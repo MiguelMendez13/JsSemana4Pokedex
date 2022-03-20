ce61edcd30b5ce004a18788bc9fff4f1ac0f33be
Mendez Flores Miguel Angel <dnmiguel.friky@gmail.com>
2022-03-19 23:31:32 -0600

    tercer commit

--- pokedex.js
+++ pokedex.js
@@ -45,6 +45,58 @@ function datosCambiar(data){
     let tiText="Tipo: "+String(data.types[0]["type"]["name"]);
 
 
+    let movimientos=document.getElementById('movimientosSelect');
+
+    let habilidades=document.getElementById('habilidadesSelect');
+
+
+    for (let i = habilidades.options.length; i >= 0; i--) {
+        habilidades.remove(i);
+    }
+    
+    const opcion = document.createElement('option');
+    textoAgregar="Habilidades";
+    opcion.value=textoAgregar;
+    opcion.text=textoAgregar;
+    habilidades.appendChild(opcion);
+
+    for(var i=0;i<data.abilities.length;i++){
+        console.log(data.abilities[i].ability["name"]);
+
+        let opcion = document.createElement('option');
+        textoAgregar=String(data.abilities[i].ability["name"]);
+        opcion.value=textoAgregar;
+        opcion.text=textoAgregar;
+        habilidades.appendChild(opcion);
+
+    }
+
+
+    for (let i = movimientos.options.length; i >= 0; i--) {
+        movimientos.remove(i);
+    }
+    
+    let opcion2 = document.createElement('option');
+    textoAgregar="Movimientos";
+    opcion2.value=textoAgregar;
+    opcion2.text=textoAgregar;
+    movimientos.appendChild(opcion2);
+
+    for(var i=0;i<data.moves.length;i++){
+        //console.log(data.moves[i].move["name"]);
+
+        const opcion = document.createElement('option');
+        textoAgregar=String(data.moves[i].move["name"]);
+        opcion.value=textoAgregar;
+        opcion.text=textoAgregar;
+        movimientos.appendChild(opcion);
+
+    }
+
+
+
+
+
 
 
     altura.innerHTML=alText;
